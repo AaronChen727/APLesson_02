ef78423232423f8c7b4ec927220fec43bf2dfb5b
AaronChen727 <halox718@gmail.com>
2016-09-13 14:29:56 -0700

    changed Concatenation

--- Concatenation.java
+++ Concatenation.java
@@ -2,9 +2,9 @@ public class Concatenation
 {
 	public static void main(String[]args)
 	{
-	String first = "12745 Via Terceto ";
-	String second = "San Diego CA, 92130 ";
-	String third = "Aaron Chen";
+	String first = "Aaron Chen ";
+	String second = "12745 Via Terceto ";
+	String third = "San Diego CA, 92130 ";
 	
 	System.out.println(first + second + third);
 	}
